35540b66266b7e00cf84c646b83ee2200539856c
hardik khanchandani <51117080+hardikhere@users.noreply.github.com>
2020-06-04 02:11:54 -0700

    Update SIngup.tsx

--- src/pages/SIngup.tsx
+++ src/pages/SIngup.tsx
@@ -7,25 +7,11 @@ import Back from '../components/Backbtn';
 const SignupPage: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-               <IonMenuButton autoHide={false}>
-               </IonMenuButton>
-          </IonButtons>
-          <IonButtons slot="primary">
-            <IonButton>
-              <IonIcon slot ="icons-only" md="ellipsis-vertical">
-              </IonIcon>
-            </IonButton>
-          </IonButtons>
-          <IonTitle>Strandard</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+     
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+           
           </IonToolbar>
         </IonHeader>
         
